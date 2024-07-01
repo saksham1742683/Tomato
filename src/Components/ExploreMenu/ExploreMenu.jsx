87a0edc246f2f594b833e8ed7f455b9087000d2e
saksham1742683 <sakshamjain1742683@gmail.com>
2024-07-01 22:54:24 +0530

    Home page complete

--- src/Components/ExploreMenu/ExploreMenu.jsx
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -1,23 +1,28 @@
-import React from 'react'
-import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets'
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
 
-
-export const ExploreMenu = () => {
+export const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div className='explore-menu' id='explore-menu' >
-        <h1>Explore Our Menu</h1>
-        <p className="explore-menu-text">
-            choose from a a diverse menu featuring a delectable array of dishes. Our mission is to is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.
-        </p>
-        <div className="explore-menu-list">
-            { menu_list.map((item , index)=>{
-                return( <div key={index} className="explore-menu-list-item">
-                    <img src={item.menu_image} alt="image" />
-                    <p>{item.menu_name} </p>
-                </div> )
-            }) }
-        </div>
+    <div className="explore-menu" id="explore-menu">
+      <h1>Explore Our Menu</h1>
+      <p className="explore-menu-text">
+        
+        choose from a a diverse menu featuring a delectable array of dishes. Our
+        mission is to is to satisfy your cravings and elevate your dining
+        experience, one delicious meal at a time.
+      </p>
+      <div className="explore-menu-list">
+        {menu_list.map((item, index) => {
+          return (
+            <div onClick={()=>setCategory(prev=> prev===item.menu_name?"All": item.menu_name ) } key={index} className="explore-menu-list-item">
+              <img className={ category===item.menu_name? "active":"" } src={item.menu_image} alt="image" />
+              <p>{item.menu_name} </p>
+            </div>
+          );
+        })}
+      </div>
+      <hr />
     </div>
-  )
-}
+  );
+};
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
 import Header from '../../Components/Header/Header'
 import { ExploreMenu } from '../../Components/ExploreMenu/ExploreMenu'
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay'
 
 export const Home = () => {
+
+    const[category , setCategory] = useState("All")
+
   return (
     <div>
         <Header/>
-        <ExploreMenu/>
+        <ExploreMenu  category={category} setCategory={setCategory} />
+        <FoodDisplay  category={category}/>
     </div>
   )
 }
