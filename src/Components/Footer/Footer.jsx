fbd6648938c067155f9468241cebcf4c076191de
saksham1742683 <sakshamjain1742683@gmail.com>
2024-08-22 19:06:40 +0530

    footer description added

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Footer.css";
+import { Link } from "react-router-dom";
 import { assets } from "../../assets/assets";
 
 const Footer = () => {
@@ -9,7 +10,11 @@ const Footer = () => {
         <div className="footer-content-left">
           <img src={assets.logo} alt="" />
           <p>
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus vitae adipisci inventore sequi. Libero, cumque nisi qui est, excepturi officiis dolor expedita nostrum voluptate ipsam adipisci, deleniti quasi id. Accusantium mollitia in eum tempora possimus.
+            TOMATO is your ultimate destination for exploring and ordering
+            mouth-watering dishes from your favorite local restaurants. We offer
+            a seamless and user-friendly online food delivery experience,
+            bringing a diverse range of cuisines right to your doorstep with
+            ease.
           </p>
           <div className="footer-social-icons">
             <img src={assets.facebook_icon} alt="" />
@@ -20,7 +25,10 @@ const Footer = () => {
         <div className="footer-content-center">
           <h2>COMPANY</h2>
           <ul>
-            <li>Home</li>
+            <li>
+              {" "}
+              <Link to="/">Home</Link>
+            </li>
             <li>About us</li>
             <li>Delivery</li>
             <li>Privacy Policy</li>
@@ -29,7 +37,7 @@ const Footer = () => {
         <div className="footer-content-right">
           <h2>GET IN TOUCH</h2>
           <ul>
-            <li>+1-212-456-9864</li>
+            <li>9999977777</li>
             <li>contact@tomato.com</li>
           </ul>
         </div>
